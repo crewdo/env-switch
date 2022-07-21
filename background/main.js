80ac912ae6a1536a3fb67d777f79fab08b8a0e52
crewdo <14520908@gm.uit.edu.vn>
2022-07-21 08:38:28 +0700

    Done

--- background/main.js
+++ background/main.js
@@ -1,67 +1,41 @@
-var XM_LOCAL_DOMAIN = 'xm.localhost';
-var XM_DEV_DOMAIN = 'dev-admin.eber.co';
-var XM_LIVE_DOMAIN = 'xm.eber.co';
-
-var APP_LOCAL_DOMAIN = 'yummypin.igift.localhost';
-var APP_DEV_DOMAIN = 'yummypin.igift.co';
-var APP_LIVE_DOMAIN = 'printskitchen.eber.co';
-
-function openIn(info, target, currentDomain, currentHref) {
-    let url;
-    let linkToOpen = info.linkUrl || currentHref
-    switch (currentDomain) {
-        case XM_LOCAL_DOMAIN:
-        case XM_DEV_DOMAIN:
-        case XM_LIVE_DOMAIN: {
-            url = xmGenerateURL(linkToOpen, target)
-            break;
-        }
-        case APP_LOCAL_DOMAIN:
-        case APP_DEV_DOMAIN:
-        case APP_LIVE_DOMAIN: {
-            url = appGenerateURL(linkToOpen, target)
-            break;
-        }
+let envMappings = [
+    {
+        platform: 'xm',
+        urls: [
+            // {
+            //     value: 'xm.localhost', env: 'local'
+            // },
+            {
+                value: 'dev-admin.eber.co', env: 'dev'
+            },
+            {
+                value: 'dev-admin-new.eber.co', env: 'dev_new'
+            },
+            {
+                value: 'xm.eber.co', env: 'live'
+            }
+        ]
+    },
+    {
+        platform: 'app',
+        urls: [
+            // {
+            //     value: 'yummypin.igift.localhost', env: 'local'
+            // },
+            {
+                value: 'yummypin.igift.co', env: 'dev'
+            },
+            {
+                value: 'yummypin.eber.co', env: 'dev_new'
+            },
+            {
+                value: 'printskitchen.eber.co', env: 'live'
+            }
+        ]
     }
+];
 
-    chrome.tabs.create({
-        url: url.replace('https://', 'http://')
-    });
-}
-
-function appGenerateURL(linkToOpen, target) {
-    var urlToReplace = (new URL(linkToOpen));
-    switch (target) {
-        case 'LOCAL':
-            urlToReplace.hostname = APP_LOCAL_DOMAIN;
-            return urlToReplace.toString();
-        case 'DEV':
-            urlToReplace.hostname = APP_DEV_DOMAIN;
-            return urlToReplace.toString();
-        case 'LIVE': {
-            urlToReplace.hostname = APP_LIVE_DOMAIN;
-            return urlToReplace.toString();
-        }
-    }
-}
-
-function xmGenerateURL(linkToOpen, target) {
-    var urlToReplace = (new URL(linkToOpen));
-    switch (target) {
-        case 'LOCAL':
-            urlToReplace.hostname = XM_LOCAL_DOMAIN;
-            return urlToReplace.toString();
-        case 'DEV':
-            urlToReplace.hostname = XM_DEV_DOMAIN;
-            return urlToReplace.toString();
-        case 'LIVE': {
-            urlToReplace.hostname = XM_LIVE_DOMAIN;
-            return urlToReplace.toString();
-        }
-    }
-}
-
-function bindingContextMenu(url) {
+function createContextMenus(url) {
 
     let parsedUrl = '';
 
@@ -71,41 +45,62 @@ function bindingContextMenu(url) {
         return;
     }
 
-    var currentDomain = parsedUrl.hostname;
-    if ([XM_LOCAL_DOMAIN, XM_DEV_DOMAIN, XM_LIVE_DOMAIN, APP_LOCAL_DOMAIN, APP_DEV_DOMAIN, APP_LIVE_DOMAIN].indexOf(currentDomain) === -1) return;
-    var currentHref = parsedUrl.href;
+    let currentHostName = parsedUrl.hostname;
+    let currentHref = parsedUrl.href;
+    let currentEnv = '';
+    let currentPlatform = '';
 
-    if (currentDomain !== XM_LOCAL_DOMAIN && currentDomain !== APP_LOCAL_DOMAIN) {
-        chrome.contextMenus.create({
-            title: "Open in LOCAL",
-            contexts: ["all"],
-            onclick: function (info) {
-                openIn(info, 'LOCAL', currentDomain, currentHref)
+    let currentMapped = envMappings.filter(function (e) {
+        return e.urls.some(function (t) {
+            if (t.value === currentHostName) {
+                currentEnv = t.env;
+                return true;
             }
         });
+    });
 
-    }
-    if (currentDomain !== XM_DEV_DOMAIN && currentDomain !== APP_DEV_DOMAIN) {
-        chrome.contextMenus.create({
-            title: "Open in DEV",
-            contexts: ["all"],
-            onclick: function (info) {
-                openIn(info, 'DEV', currentDomain, currentHref)
-            }
-        });
+    if (currentMapped.length > 0) {
+        currentPlatform = currentMapped[0].platform;
+    } else {
+        return;
     }
 
-    if (currentDomain !== XM_LIVE_DOMAIN && currentDomain !== APP_LIVE_DOMAIN) {
-        chrome.contextMenus.create({
-            title: "Open in LIVE",
-            contexts: ["all"],
-            onclick: function (info) {
-                openIn(info, 'LIVE', currentDomain, currentHref)
-            }
-        });
-    }
+    currentMapped[0].urls.map(function (e) {
+        if(e.value !== currentHostName) {
+
+            let intendedUrl = generateIntendedUrl(currentHref, e.value);
+
+            chrome.contextMenus.create({
+                title: "Open in " + currentPlatform.toUpperCase() + " " + e.env.toUpperCase(),
+                contexts: ["all"],
+                onclick: function () {
+                    openTab(intendedUrl);
+                }
+            });
+        }
+    })
 }
 
+function generateIntendedUrl(currentHref, newHost)
+{
+    let parsedUrl = '';
+
+    try {
+        parsedUrl = new URL(currentHref);
+    } catch (e) {
+        return;
+    }
+
+    parsedUrl.hostname = newHost;
+
+    return parsedUrl.toString();
+}
+
+function openTab(intendedUrl) {
+    chrome.tabs.create({
+        url: intendedUrl.replace('https://', 'http://')
+    });
+}
 
 function getCurrentTabUrl(callback) {
 
@@ -122,12 +117,12 @@ function getCurrentTabUrl(callback) {
 
 chrome.tabs.onActivated.addListener(function () {
     getCurrentTabUrl(function (url) {
-        bindingContextMenu(url);
+        createContextMenus(url);
     });
 });
 
 chrome.tabs.onUpdated.addListener(function () {
     getCurrentTabUrl(function (url) {
-        bindingContextMenu(url);
+        createContextMenus(url);
     });
 });
